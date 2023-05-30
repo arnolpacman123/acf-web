74f1499cb3893aceb211223a06f1c85d6abca4ff
Vane737 <vivian_vanessa_flores@hotmail.com>
2023-05-30 14:45:34 -0400

    Control de errores al listar los campeonatos

--- src/app/modules/private/services/acf.data.service.ts
+++ src/app/modules/private/services/acf.data.service.ts
@@ -1,24 +1,33 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment.prod';
-import { Observable } from 'rxjs';
+import Swal from 'sweetalert2';
 
 
 @Injectable()
 
-export class AcfService {
+export class AcfDataService {
 
   constructor( private http : HttpClient ) {
-    console.log("acfService Listo");
+    //console.log("acfService Listo");
    }
 
-   getQuery( query : string ) {
-     console.log(`${environment.apiBaseUrl}${query}`);
+    getQuery( query : string ) {
+      console.log(`${environment.apiBaseUrl}${query}`);
         const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access_token'));
         return this.http.get(`${environment.apiBaseUrl}${query}`, { headers });
-   }
-
-   getChampionsAll() {
+    }
+    getChampionsAll() {
       return this.getQuery('/championships');
-   }
+    }
+
+    showMessageDialog(title: string, text: string, icon: any) {
+      Swal.fire({
+        title,
+        text,
+        icon,
+        confirmButtonText: 'Aceptar',
+        timer: 30000,
+      });
+    }
 }
